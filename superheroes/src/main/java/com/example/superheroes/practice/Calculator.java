91917670800fceee192cfbe50c04a72e80e98ae2
siot12 <tudorciot@gmail.com>
2023-07-28 12:30:14 +0100

    new commit

---
+++ superheroes/src/main/java/com/example/superheroes/practice/Calculator.java
@@ -0,0 +1,11 @@
+package com.example.superheroes.practice;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculator {
+    public int addition(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+}
